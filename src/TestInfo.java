53dbd5c536c551605e713f3c153d58ca6d4dceb8
Sym Conger <acesilverryuu@gmail.com>
2019-04-23 12:15:56 -0500

    paradise

---
+++ src/TestInfo.java
@@ -0,0 +1,8 @@
+public class TestInfo {
+
+    public static void main (String[] args){
+
+        System.out.println("Calling mehod from another class:");
+        ParadiseInfo.displayInfo();
+    }
+}
