554bd5cbf057f01c1c761efc91a03c16ae0cfc16
Sym Conger <acesilverryuu@gmail.com>
2019-04-23 12:08:31 -0500

    liters

--- src/MetricConversion.java
+++ src/MetricConversion.java
@@ -5,13 +5,19 @@ public class MetricConversion {
     public static void main (String [] args){
 
         int inch;
+        int gallon;
 
         Scanner input = new Scanner (System.in);
 
-        System.out.print("Please enter a number >>>>");
+        System.out.print("Please enter a number of inches >>>>");
         inch = input.nextInt();
 
+        System.out.print("Please enter a number of gallons >>>>");
+        gallon = input.nextInt();
+
         inches(inch);
+
+        liters(gallon);
     }
 
     public static void inches (int inch){
@@ -23,5 +29,14 @@ public class MetricConversion {
 
 
         System.out.println("The number of centimeters in your inches is "+ centin);
+    }
+    public static void liters(int gallon){
+
+        double liter = 3.7854;
+        double galler;
+
+        galler = gallon * liter;
+
+        System.out.println("The number of liters in your gallon(s) is "+ galler);
     }
 }
